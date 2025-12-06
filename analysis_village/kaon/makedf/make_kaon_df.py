4740ef56e65e1bd70feda01907b1d41c4f64155e
Thomas Wester <twester@uchicago.edu>
2025-12-06 10:24:55 -0600

    save daughter info for all primaries

--- analysis_village/kaon/makedf/make_kaon_df.py
+++ analysis_village/kaon/makedf/make_kaon_df.py
@@ -1,6 +1,7 @@
 """
 Kaon analysis data frame maker
 """
+import functools
 
 import numpy as np
 import pandas as pd
@@ -31,10 +32,14 @@ PRIM_BRANCHES = list(set(branches.mcprimbranches + [
 ]))
 
 
+# InFV requires inzback but it does nothing for SBND case
+# put NaN here so we'll hopefully get an error if this ever changes
+InFV_SBND = functools.partial(util.InFV, inzback=np.nan, det='SBND')
+
+
 def signal(df: pd.DataFrame, ktype: str, cc: bool=True) -> pd.DataFrame:
-    # InFV requires inzback but it does nothing for SBND case
-    # put NaN here so we'll hopefully get an error if this ever changes
-    is_true_fv = util.InFV(df.position, inzback=np.nan, det='SBND')
+    """Signal definition for mcdf."""
+    is_true_fv = InFV_SBND(df.position, inzback=np.nan, det='SBND')
 
     # TODO daughter particle containment cut
 
@@ -48,9 +53,19 @@ def make_kaon_mcdf(f: pd.DataFrame) -> pd.DataFrame:
 
     mcprimdf = ph.loadbranches(f["recTree"], PRIM_BRANCHES).rec.mc.nu.prim
 
-    # for daughter info, will match on entry + G4ID
+    # daughter info
     tpartdf = ph.loadbranches(f["recTree"], branches.trueparticlebranches).rec.true_particles
     tpartdf = tpartdf.reset_index().set_index(['entry', 'G4ID'])
+
+    mcprimdaughtersdf = makedf.make_mcprimdaughtersdf(f).rec.mc.nu.prim
+    daughterdf = mcprimdaughtersdf[mcprimdaughtersdf.index.droplevel(-1).isin(mcprimdf.index)]
+    daughter_tpartdf = tpartdf[
+        tpartdf.index.isin(pd.MultiIndex.from_frame(daughterdf.reset_index()[['entry', 'daughters']]))
+    ]
+    daughterdf = ph.multicol_merge(daughterdf, daughter_tpartdf, how="left", left_on=['entry', 'daughters'], right_index=True)
+    mcprimdf = ph.multicol_add(mcprimdf, daughterdf.groupby(level=[0, 1, 2]).size().rename('ndaughters'))
+
+    mcdf = ph.multicol_merge(mcdf, mcprimdf, how="left", left_index=True, right_index=True, validate="one_to_one")
 
     # add kaon info: Number above threshold & primary info
     for kname in ('kplus', 'kzero'):
@@ -59,21 +74,13 @@ def make_kaon_mcdf(f: pd.DataFrame) -> pd.DataFrame:
         mcdf = ph.multicol_add(mcdf, ((mcprimdf.pdg==KPDG[kname]) \
                                               & (ke > TRUE_KE_CUT)).groupby(level=[0, 1]).sum().rename(f'n{kname}'))
 
+        '''
         # kaon primary info
         kdf = mcprimdf[mcprimdf.pdg==KPDG[kname]]
 
-
-        # kaon daughter info
-        mcprimdaughtersdf = makedf.make_mcprimdaughtersdf(f).rec.mc.nu.prim
-        kdaughterdf = mcprimdaughtersdf[mcprimdaughtersdf.index.droplevel(-1).isin(kdf.index)]
-        kdaughter_tpartdf = tpartdf[
-            tpartdf.index.isin(pd.MultiIndex.from_frame(kdaughterdf.reset_index()[['entry', 'daughters']]))
-        ].reset_index().set_index(['entry', 'parent'])
-
-        print(kdaughter_tpartdf.groupby(level=[0, 1]).size().rename('ndaughters'))
-
         kdf.columns = pd.MultiIndex.from_tuples([tuple([kname] + list(c)) for c in kdf.columns])
         mcdf = ph.multicol_merge(mcdf, kdf, how="left", left_index=True, right_index=True, validate="one_to_one")
+        '''
 
     mcdf['is_signal_kp_cc'] = signal(mcdf, ktype='kplus', cc=True)
     mcdf['is_signal_kp_nc'] = signal(mcdf, ktype='kplus', cc=False)
@@ -85,7 +92,10 @@ def make_kaon_recodf(f: pd.DataFrame) -> pd.DataFrame:
     pandora_df = makedf.make_pandora_df(f)
 
     # precuts
-    pandora_df = pandora_df[util.InFV(pandora_df.slc.vertex, inzback=np.nan, det='SBND')]
+    pandora_df = pandora_df[InFV_SBND(pandora_df.slc.vertex, inzback=np.nan, det='SBND')]
     pandora_df = pandora_df[pandora_df.slc.is_clear_cosmic == 0]
+
+    # daughter info
+    daughterdf = ph.loadbranches(f["recTree"], branches.pfp_daughter_branch)
 
     return pandora_df
